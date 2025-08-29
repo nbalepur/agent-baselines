2d7e9db499f979cd0082c7c141fc0e294744c159
nbalepur <nbalepur@umd.edu>
2025-08-29 08:46:53 -0700

    black formatting

--- agent_baselines/solvers/sqa/formatted_perplexity.py
+++ agent_baselines/solvers/sqa/formatted_perplexity.py
@@ -8,6 +8,7 @@ from agent_baselines.solvers.sqa.perplexity_base import perplexity_solver
 
 logger = logging.getLogger(__name__)
 
+
 @solver
 def add_perplexity_references() -> Solver:
 
@@ -21,12 +22,13 @@ def add_perplexity_references() -> Solver:
         references_str = '\n\n## References\n\nEach reference below is in the format "[citation ID] {title} ({url})". Use `title` as the `excerpt`'
         for idx, citation in enumerate(citations):
             title, url = citation.title, citation.url
-            references_str += f'\n[{idx + 1}] {title} ({url})'
+            references_str += f"\n[{idx + 1}] {title} ({url})"
 
         state.messages.append(ChatMessageAssistant(content=m.text + references_str))
         return state
 
     return solve
+
 
 @solver
 def formatted_solver(
@@ -35,7 +37,7 @@ def formatted_solver(
     reasoning_effort: str = "",
     search_mode: str = "",
     require_snippets: bool = True,
-    scorer_model: str = "google/gemini-2.5-flash-preview-05-20"
+    scorer_model: str = "google/gemini-2.5-flash-preview-05-20",
 ) -> Solver:
     chainlist = [
         perplexity_solver(
@@ -44,7 +46,7 @@ def formatted_solver(
             system_message=system_prompt,
             search_context_size=search_context_size,
             reasoning_effort=reasoning_effort,
-            search_mode=search_mode
+            search_mode=search_mode,
         ),
         add_perplexity_references(),
         format_solver(scorer_model, require_snippets=require_snippets),
